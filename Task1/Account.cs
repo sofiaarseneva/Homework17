8ec87945d5b67f03a8a7e883cb1d6d19656c3e4b
Sofia <151773@herzen.spb.ru>
2024-10-21 14:07:45 +0300

    Добавьте файлы проекта.

---
+++ Task1/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Account<T>
+    {
+        T Num { get; set; }
+        string Fio { get; set; }
+        double Balance { get; set; }
+
+        public void setInfo()
+        {
+            Console.WriteLine("Введите номер счета:");
+            Num = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите ФИО владельца:");
+            Fio = Console.ReadLine();
+            Console.WriteLine("Введите баланс счета:");
+            Balance = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public string getInfo()
+        {
+            return $"\nНомер счета - {Num}.\nФИО владельца - {Fio}.\nБаланс счета - {Balance}.";
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Обобщенные типы - моделирование счета\n\n");
+            try
+            {
+                Console.WriteLine("Тип номера счета - число!");
+                Account<int> accountInt = new Account<int>();
+                accountInt.setInfo();
+                Console.WriteLine(accountInt.getInfo());
+                Console.WriteLine("\nТип номера счета - строка!");
+                Account<string> accountStr = new Account<string>();
+                accountStr.setInfo();
+                Console.WriteLine(accountStr.getInfo());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка!" + ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
